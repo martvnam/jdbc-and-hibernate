f731ff3a8d497bc432adc5ce62372a4a27f4ba91
Aleksandra <aleksandraleszczynska@hotmail.com>
2023-03-04 11:27:02 +0100

    [change] - more output to console

--- src/main/java/pl/sda/orange2/jah/Main.java
+++ src/main/java/pl/sda/orange2/jah/Main.java
@@ -3,6 +3,7 @@ package pl.sda.orange2.jah;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Works..");
+        System.out.println("JDBC and Hibernate - love it! ");
     }
 
 }
